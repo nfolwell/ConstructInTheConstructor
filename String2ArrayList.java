ef28d82fe01acd61509f0e6569d1d1ea2a1b30c0
BTHS Java A Class - Mr. F <nfolwell@schools.nyc.gov>
2023-03-06 08:06:03 -0500

    Add files via upload

---
+++ String2ArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class String2ArrayList
+{
+    private ArrayList<String> splitString;
+    
+    public String2ArrayList()
+    {
+        //Construct the splitString here!
+    }
+    public void splitAndAdd(String str)
+    {
+        //split the string into single character strings 
+        //and add them ot splitString
+    }
+    public String toString()
+    {
+        return "";
+    }
+}
